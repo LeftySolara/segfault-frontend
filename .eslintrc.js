63dc9b1f74bb7259f0ea836ba90fad5eb8416bb2
Julianne Adams <julianne@julianneadams.info>
2022-07-14 20:38:48 -0400

    Initial project setup

---
+++ .eslintrc.js
@@ -0,0 +1,60 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "airbnb",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+      modules: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier", "jest"],
+  rules: {
+    "prettier/prettier": ["error", { singleQuote: false }],
+    "react/jsx-filename-extension": [
+      1,
+      { extensions: [".js", ".jsx", "tsx", ".ts"] },
+    ],
+    "react/function-component-definition": [
+      2,
+      { namedComponents: "arrow-function" },
+    ],
+    "no-plusplus": "off",
+    "global-require": 0,
+    "no-underscore-dangle": "off",
+    quotes: [2, "double"],
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never",
+      },
+    ],
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        paths: ["src"],
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
+};
