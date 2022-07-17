43ca2a7d266677eed5294ccfb106a998544d80ce
Julianne Adams <julianne@julianneadams.info>
2022-07-17 11:47:40 -0400

    Add: theme settings

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
 
-import "./index.css";
+import { MantineProvider } from "@mantine/core";
+
+import theme from "./assets/theme";
+import App from "./App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 root.render(
   <BrowserRouter>
-    <App />
+    <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
+      <App />
+    </MantineProvider>
   </BrowserRouter>,
 );
