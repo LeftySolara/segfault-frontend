bdd84a40a41b77d0251071831240ba73b126403c
Julianne Adams <julianne@julianneadams.info>
2022-07-24 09:47:02 -0400

    Refactor: copy form base from signup page and remove extra inputs

---
+++ src/pages/Login/LoginForm/LoginForm.styles.ts
@@ -0,0 +1,65 @@
+import { createStyles } from "@mantine/core";
+
+const useFormStyles = createStyles((theme) => ({
+  "form-container": {
+    position: "relative",
+    width: "640px",
+    height: "768px",
+    maxWidth: "640px",
+    maxHeight: "768px",
+    backgroundColor: "white",
+    display: "flex",
+    flexFlow: "column nowrap",
+    justifyContent: "center",
+    alignItems: "center",
+    boxShadow: `10px 10px 20px ${theme.colors["cool-grey"][3]}`,
+    borderRadius: "20px",
+  },
+  form: {
+    width: "396px",
+    fontSize: "20px",
+  },
+  button: {
+    width: "128px",
+    height: "55px",
+    color: theme.colors.purple[0],
+    backgroundColor: theme.colors.purple[4],
+    borderRadius: 50,
+    "&:hover": {
+      backgroundColor: theme.colors.purple[4],
+    },
+  },
+  link: {
+    textDecoration: "none",
+    fontFamily: "soleil",
+    color: theme.colors.purple[4],
+  },
+  "text-input": {
+    width: "100%",
+    height: "64px",
+    marginBottom: "48px",
+    color: theme.colors["cool-grey"][6],
+    backgroundColor: theme.colors["cool-grey"][0],
+    border: "none",
+    borderRadius: "10px",
+    boxShadow: `3px 3px 5px ${theme.colors["cool-grey"][2]}`,
+  },
+  label: {
+    fontFamily: "soleil",
+    fontSize: "20px",
+    color: theme.colors.purple[4],
+  },
+  asterisk: {
+    display: "none",
+  },
+  error: {
+    position: "absolute",
+    padding: "0px 0px 20px 0px",
+    translate: "0px -45px",
+    color: theme.colors.red[4],
+    backgroundColor: "white",
+    zIndex: 2,
+  },
+}));
+
+export default useFormStyles;
---
+++ src/pages/Login/LoginForm/LoginForm.tsx
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import {
+  Box,
+  Button,
+  Group,
+  LoadingOverlay,
+  PasswordInput,
+  TextInput,
+} from "@mantine/core";
+import { useForm } from "@mantine/form";
+
+import { loginUser } from "api/user";
+
+import useFormStyles from "./LoginForm.styles";
+
+interface FormValues {
+  email: string;
+  password: string;
+}
+
+const LoginForm = (): JSX.Element => {
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (values: FormValues) => {
+    setLoading(true);
+    const response = await loginUser(values.email, values.password);
+    setLoading(false);
+
+    if (response && response.status === 200) {
+      navigate("/", { replace: false });
+    }
+  };
+
+  const form = useForm({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const { classes } = useFormStyles();
+
+  return (
+    <Box className={classes["form-container"]}>
+      <LoadingOverlay visible={loading} />
+      <form
+        className={classes.form}
+        onSubmit={form.onSubmit((values) => handleSubmit(values))}
+      >
+        <TextInput
+          required
+          id="email-input"
+          name="email-input"
+          type="email"
+          label="Email"
+          placeholder="your@email.com"
+          classNames={{
+            input: classes["text-input"],
+            label: classes.label,
+            required: classes.asterisk,
+            error: classes.error,
+            invalid: classes["text-input"],
+          }}
+          {...form.getInputProps("email")}
+        />
+        <PasswordInput
+          required
+          id="password-input"
+          name="password-input"
+          label="Password"
+          classNames={{
+            innerInput: classes["text-input"],
+            input: classes["text-input"],
+            label: classes.label,
+            required: classes.asterisk,
+            error: classes.error,
+            invalid: classes["text-input"],
+          }}
+          {...form.getInputProps("password")}
+        />
+        <Group position="apart" mt="md">
+          <Link to="/signup" className={classes.link}>
+            Sign up Instead
+          </Link>
+          <Button type="submit" className={classes.button}>
+            Log In
+          </Button>
+        </Group>
+      </form>
+    </Box>
+  );
+};
+
+export default LoginForm;
