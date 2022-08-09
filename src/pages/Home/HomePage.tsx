3ac608e3f27a9fa9170c1f18a9b4d4e705b11353
Julianne Adams <julianne@julianneadams.info>
2022-08-09 15:57:13 -0400

    Remove: commented-out code

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -62,39 +62,6 @@ const HomePage = (): JSX.Element => {
     }
   }, [fetchedBoards]);
 
-  /*
-  const matchBoardsToCategories = () => {
-    let categoryBoards;
-    let boardCategory;
-
-    const categories: Array<BoardCategory> = [];
-
-    sampleBoardCategories.forEach((category) => {
-      categoryBoards = sampleBoards
-        .filter((board) => board.category.id === category.id)
-        .map((board) => {
-          return {
-            id: board._id,
-            topic: board.topic,
-            description: board.description,
-            threadCount: board.threads.length,
-          };
-        });
-
-      boardCategory = {
-        id: category.id,
-        topic: category.topic,
-        boards: categoryBoards,
-      };
-
-      // categories.push(boardCategory);
-    });
-
-    return categories;
-  };
-*/
-  // const categories = matchBoardsToCategories();
-
   return (
     <div>
       <LoadingOverlay visible={isLoading} />
