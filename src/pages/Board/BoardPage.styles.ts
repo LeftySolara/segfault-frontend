76b631c70d14ebfa24b3609d89be4e660feb404f
Julianne Adams <julianne@julianneadams.info>
2022-08-16 19:52:35 -0400

    Add: breadcrumbs

---
+++ src/pages/Board/BoardPage.styles.ts
@@ -0,0 +1,12 @@
+import { createStyles } from "@mantine/core";
+
+const useBoardPageStyles = createStyles((theme) => ({
+  anchor: {
+    fontFamily: "soleil",
+    fontSize: "24px",
+    color: theme.colors["cool-grey"][5],
+    marginBottom: "30px",
+  },
+}));
+
+export default useBoardPageStyles;
--- src/pages/Board/BoardPage.tsx
+++ src/pages/Board/BoardPage.tsx
@@ -1,11 +1,37 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Breadcrumbs, Text } from "@mantine/core";
+import { Link, useParams } from "react-router-dom";
+import { useGetBoardByIdQuery } from "services/board";
 import ThreadList from "./ThreadList/ThreadList";
+
+import useBoardPageStyles from "./BoardPage.styles";
 
 const BoardPage = () => {
   const { id } = useParams();
+  const { classes } = useBoardPageStyles();
 
-  return <div>{id && <ThreadList boardId={id} />}</div>;
+  const { data: fetchedBoard } = useGetBoardByIdQuery({ id });
+
+  const crumbs = [
+    { title: "Index", href: "/" },
+    {
+      title: fetchedBoard?.board.topic ? `${fetchedBoard.board.topic}` : "",
+      href: `/board/${id}`,
+    },
+  ].map((item, index) => {
+    return (
+      <Text component={Link} to={item.href} key={index === 0 ? 0 : id}>
+        {item.title}
+      </Text>
+    );
+  });
+
+  return (
+    <div>
+      <Breadcrumbs className={classes.anchor}>{crumbs}</Breadcrumbs>
+      {id && <ThreadList boardId={id} />}
+    </div>
+  );
 };
 
 export default BoardPage;
