4b7b71123d70363db46e61a0a67a48718f7d9c38
Julianne Adams <julianne@julianneadams.info>
2022-08-17 12:30:46 -0400

    Add: query for fetching all posts in a thread

--- src/services/post.ts
+++ src/services/post.ts
@@ -1,3 +1,5 @@
+import api from "./api";
+
 export interface Post {
   id: string;
   author: {
@@ -12,3 +14,28 @@ export interface Post {
   dateCreated: string;
   content: string;
 }
+
+export interface GetPostsByThreadResponse {
+  posts: Array<Post>;
+}
+
+const postApi = api.injectEndpoints({
+  endpoints: (builder) => ({
+    getPostsByThread: builder.query<
+      GetPostsByThreadResponse,
+      { id: string | undefined }
+    >({
+      query: (arg) => {
+        const { id } = arg;
+        return {
+          url: `/posts/thread/${id}`,
+          method: "GET",
+          credentials: "include",
+        };
+      },
+    }),
+  }),
+  overrideExisting: false,
+});
+
+export const { useGetPostsByThreadQuery } = postApi;
